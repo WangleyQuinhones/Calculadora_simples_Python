5c379a19b67611b87b4516b95f2af3daa639cf04
WangleyQuinhones <wangleyquinhones@gmail.com>
2024-01-09 19:01:03 -0400

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,15 @@
+# Importando tkinter
+from tkinter import *
+from tkinter import ttk
+
+#cores
+cor1 = "#3b3b3b" #preta
+cor2 = "#feffff" #branca
+cor3 = "#38576b" #azul
+cor4 = "#eceff1" #cinza
+cor5 = "#ffab40" #laranja
+
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("235x310")
+janela.config(bg=cor1)
